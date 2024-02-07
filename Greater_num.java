a73881050b8aed777cdfd57b8aa3b37808a1f9d3
Shivishun <128620193+Shivishun@users.noreply.github.com>
2024-02-07 14:55:16 +0530

    Add files via upload

---
+++ Greater_num.java
@@ -0,0 +1,11 @@
+public class Greater_num {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=5;
+        int c=7;
+        if(a>b && a>c){
+            System.out.println("The greatest number is: "+a);
+        }
+    }
+}
---
+++ lowercase.java
@@ -0,0 +1,7 @@
+public class lowercase {
+    public static void main(String[] args) {
+        String s="My name is mike";
+        String c=s.toLowerCase();
+        System.out.println(c);
+    }
+}
