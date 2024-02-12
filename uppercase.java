bd18076f5c97cfbbdd527662fbf282de0aa00f7e
Shivishun <128620193+Shivishun@users.noreply.github.com>
2024-02-12 14:54:12 +0530

    Delete uppercase.java

--- uppercase.java
+++
@@ -1,7 +0,0 @@
-public class uppercase {
-    public static void main(String[] args) {
-        String s="My name is mike";
-        String c=s.toUpperCase();
-                System.out.println(c);
-    }
-}
