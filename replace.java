7cc2c4b94c065df644911410661d6ae11e46a64a
Shivishun <128620193+Shivishun@users.noreply.github.com>
2024-02-12 14:53:44 +0530

    Delete replace.java

--- replace.java
+++
@@ -1,7 +0,0 @@
-public class replace {
-    public static void main(String[] args) {
-        String s="My name is mike";
-        String c=s.replace("i","j");
-        System.out.println(c);
-    }
-}
